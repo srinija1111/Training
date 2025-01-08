9bceae7e4ff93a8d054b8c261a97cbc37486d391
sriija1111 <2211cs010544@mallareddyuniversity.ac.in>
2025-01-08 10:27:19 +0530

    looping and function introduction done

---
+++ evenodd.js
@@ -0,0 +1,9 @@
+// Printing even and odd numbers from 1 to 100
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+      console.log("Even: " + i);
+    } else {
+      console.log("Odd: " + i);
+    }
+  }
+  
