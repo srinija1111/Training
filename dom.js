3071aad8b6f6d3bd2e55638e5519c17e2417a27f
sriija1111 <2211cs010544@mallareddyuniversity.ac.in>
2025-01-24 12:58:40 +0530

    pushing files to repository

--- dom.js
+++ dom.js
@@ -1,2 +1,29 @@
-let element = document.getElementById("demo");
-console.log(element);
+// let element = document.getElementById("demo");
+// console.log(element);
+// let ele = document.getElementsByClassName("test");
+//  console.log(ele);
+//  console.log(Array.isArray(ele));
+//  ele[0].innerText="hello"
+// //  ele[0].style.backgroundColor="tomato"
+
+//  let x=[...ele];
+//  x.map(element=>{
+//     element.style.backgroundColor="pink" 
+//  })
+ 
+// let ele=document.querySelectorAll(".test");
+// console.log(ele);
+// let btn=document.querySelector("#btn");
+// btn.addEventListener("dblclick",()=>{
+//     console.log("double clicked");
+// })
+let divBlock=document.getElementById("divBlock");
+divBlock.addEventListener("mouseover",()=>{
+    document.body.style.backgroundColor="yellow";
+    divBlock.style.backgroundColor="blue"
+    
+});
+divBlock.addEventListener("mouseleave",()=>{
+    document.body.style.backgroundColor="orange"
+    divBlock.style.backgroundColor="aqua"
+})
